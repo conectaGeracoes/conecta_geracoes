5ea4148fbff99bc3e16d2032653da5e4643fbb18
unknown <marcondesnicole4@gmail.com>
2026-07-23 23:23:07 -0300

    ajustes na home

--- js/loadComponents.js
+++ js/loadComponents.js
@@ -4,3 +4,10 @@ fetch("/components/header.html")
     .then(data => {
         document.getElementById("header").innerHTML = data;
     });
+
+    fetch("./components/chatbot.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("chatbot-container").innerHTML = data;
+        iniciarChatbot();
+    });
